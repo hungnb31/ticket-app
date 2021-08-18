d24c66e8150c0ed1f0e962ed6a60171bd0e400ae
Hung Nguyen Ba <nbhung189@gmail.com>
2021-08-18 23:10:13 +0700

    update ingress service and routers for auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,23 @@
 import * as express from "express";
 import { json } from "body-parser";
 
+import { currentUserRouter } from "./routes/current-user";
+import { signInRouter } from "./routes/signin";
+import { signOutRouter } from "./routes/signout";
+import { signUpRouter } from "./routes/signup";
+
 const app = express();
 app.use(json());
 
+app.use(currentUserRouter);
+app.use(signInRouter);
+app.use(signOutRouter);
+app.use(signUpRouter);
+
+app.get("/api/users/currentuser", (req, res) => {
+  res.send("Hi there!");
+});
+
 app.listen(3000, () => {
-  console.log("Auth Service is listening on port 3000");
+  console.log("Auth Service is listening on port 3000!!!");
 });
