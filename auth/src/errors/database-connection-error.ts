770619776b145acd216d76f7d76ed353118951ee
Hung Nguyen Ba <nbhung189@gmail.com>
2021-08-20 23:21:53 +0700

    updated on error handling

--- auth/src/errors/database-connection-error.ts
+++ auth/src/errors/database-connection-error.ts
@@ -1,8 +1,9 @@
-export class DatabaseConnectionError extends Error {
+import { CustomError } from "./custom-error";
+export class DatabaseConnectionError extends CustomError {
   statusCode = 500;
   reason = "Error while connecting to database";
   constructor() {
-    super();
+    super("Error while connecting to database");
 
     Object.setPrototypeOf(this, DatabaseConnectionError.prototype);
   }
