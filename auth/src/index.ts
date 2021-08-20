f4d23d2af607b509761cfaf8a3672f3ea23c1c97
Hung Nguyen Ba <nbhung189@gmail.com>
2021-08-20 23:05:13 +0700

    add customer errors

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,9 @@ import { currentUserRouter } from "./routes/current-user";
 import { signInRouter } from "./routes/signin";
 import { signOutRouter } from "./routes/signout";
 import { signUpRouter } from "./routes/signup";
+import { errorHandler } from "./middlewares/error-handler";
+import { RequestValidationError } from "./errors/request-validation-error"; 
+import { DatabaseConnectionError } from "./errors/database-connection-error"; 
 
 const app = express();
 app.use(json());
@@ -13,6 +16,8 @@ app.use(currentUserRouter);
 app.use(signInRouter);
 app.use(signOutRouter);
 app.use(signUpRouter);
+
+app.use(errorHandler);
 
 app.get("/api/users/currentuser", (req, res) => {
   res.send("Hi there!");
--- auth/src/routes/signup.ts
+++ auth/src/routes/signup.ts
@@ -1,6 +1,8 @@
 import { Response, Request } from "express";
 import * as express from "express";
 import { body, validationResult } from "express-validator";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
 
 const router = express.Router();
 
@@ -17,10 +19,14 @@ router.post(
     const errors = validationResult(req);
 
     if (!errors.isEmpty()) {
-      return res.status(400).send(errors.array());
+      throw new RequestValidationError(errors.array());
     }
     const { email, password } = req.body;
-    res.send("Hi there!");
+
+    console.log("Creating user...");
+    throw new DatabaseConnectionError();
+
+    res.send({});
   }
 );
 
