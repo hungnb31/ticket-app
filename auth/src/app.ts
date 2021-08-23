591d7083389f8e8d46e46d407b989187ff6ca295
Hung Nguyen Ba <nbhung189@gmail.com>
2021-08-23 22:36:33 +0700

    update more tests for signup

--- auth/src/app.ts
+++ auth/src/app.ts
@@ -16,7 +16,7 @@ app.set("trust proxy", 1);
 app.use(
   cookieSession({
     signed: false,
-    secure: true,
+    secure: process.env.NODE_ENV !== "test",
   })
 );
 app.use(json());
--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -33,3 +33,24 @@ it("returns a 400 with missing email and password", async () => {
     .send({ password: "password" })
     .expect(400);
 });
+
+it("disallows duplicate email", async () => {
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "password" })
+    .expect(201);
+
+  await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "password" })
+    .expect(400);
+});
+
+it("set a cookie after successful signup", async () => {
+  const response = await request(app)
+    .post("/api/users/signup")
+    .send({ email: "test@test.com", password: "password" })
+    .expect(201);
+
+  expect(response.get("Set-Cookie")).toBeDefined();
+});
