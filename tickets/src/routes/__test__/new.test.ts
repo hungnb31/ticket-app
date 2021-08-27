a63291763f3f0ff57ed9bc9bdc5c90bb42c7f20f
Hung Nguyen Ba <nbhung189@gmail.com>
2021-08-27 16:50:47 +0700

    test first approach

---
+++ tickets/src/routes/__test__/new.test.ts
@@ -0,0 +1,13 @@
+import request from "supertest";
+
+import { app } from "../../app";
+
+it("has a route handler listening to /api/tickets for post request", async () => {});
+
+it("can only be access if user is signed in", async () => {});
+
+it("returns an error if an invalid title is provided", async () => {});
+
+it("returns an error if an invalid price is provided", async () => {});
+
+it("creates a ticket with valid input", async () => {});
