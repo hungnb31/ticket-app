7d1666c0965cd9ccba2ddec167bbf696f6b761a6
Hung Nguyen Ba <nbhung189@gmail.com>
2021-08-27 16:46:00 +0700

    move mongo uri to env

--- tickets/src/index.ts
+++ tickets/src/index.ts
@@ -6,13 +6,16 @@ const start = async () => {
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
+  if (!process.env.MONGO_URI) {
+    throw new Error("MONGO_URI must be defined");
+  }
   try {
-    await mongoose.connect("mongodb://tickets-mongo-srv:27017/tickets", {
+    await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
       useCreateIndex: true,
     });
-    console.log("Connected to mongodb!");
+    console.log("Connected to MongoDB!");
   } catch (error) {
     console.error(error);
   }
