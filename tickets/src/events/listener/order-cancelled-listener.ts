b141fadb8a7ff5e120235e22ac1a74dbc5bebd71
Hung Nguyen Ba <nbhung189@gmail.com>
2021-09-04 23:37:23 +0700

    update order cancelled listener

---
+++ tickets/src/events/listener/order-cancelled-listener.ts
@@ -0,0 +1,33 @@
+import { Listener, OrderCancelledEvent, Subjects } from "@ticketappdev/common";
+import { Message } from "node-nats-streaming";
+
+import { queueGroupName } from "./queue-group-name";
+import { Ticket } from "../../models/tickets";
+import { TicketUpdatedPublisher } from "../publisher/ticket-updated-publisher";
+
+export class OrderCancelledListener extends Listener<OrderCancelledEvent> {
+  readonly subject = Subjects.OrderCancelled;
+
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCancelledEvent["data"], msg: Message) {
+    const ticket = await Ticket.findById(data.ticket.id);
+
+    if (!ticket) throw new Error("Ticket not found!");
+
+    ticket.set({ orderId: undefined });
+
+    await ticket.save();
+
+    await new TicketUpdatedPublisher(this.client).publish({
+      id: ticket.id,
+      orderId: ticket.orderId,
+      userId: ticket.userId,
+      price: ticket.price,
+      title: ticket.title,
+      version: ticket.version,
+    });
+
+    msg.ack();
+  }
+}
