ab82ca95b675d405ae9d4530b551415508fa0887
Hung Nguyen Ba <nbhung189@gmail.com>
2021-09-05 18:34:48 +0700

    setup first route for payments service

--- payments/src/app.ts
+++ payments/src/app.ts
@@ -4,6 +4,8 @@ import { json } from "body-parser";
 import cookieSession from "cookie-session";
 import { errorHandler, NotFoundError, currentUser } from "@ticketappdev/common";
 
+import { createChargeRouter } from "./routes/new";
+
 const app = express();
 
 app.set("trust proxy", 1);
@@ -15,6 +17,7 @@ app.use(
 );
 app.use(json());
 app.use(currentUser);
+app.use(createChargeRouter);
 
 app.all("*", async () => {
   throw new NotFoundError();
