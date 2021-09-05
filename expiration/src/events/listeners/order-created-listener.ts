5e93da809fc81f77256298fcfffe00cca5c36f86
Hung Nguyen Ba <nbhung189@gmail.com>
2021-09-05 11:21:20 +0700

    create bull queue when received order created event

---
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -0,0 +1,15 @@
+import { Listener, OrderCreatedEvent, Subjects } from "@ticketappdev/common";
+import { Message } from "node-nats-streaming";
+import { queueGroupName } from "./queue-group-name";
+import { expirationQueue } from "../../queues/expiration-queue";
+
+export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
+  readonly subject = Subjects.OrderCreated;
+  queueGroupName = queueGroupName;
+
+  async onMessage(data: OrderCreatedEvent["data"], msg: Message) {
+    await expirationQueue.add({ orderId: data.id });
+
+    msg.ack();
+  }
+}
---
+++ expiration/src/queues/expiration-queue.ts
@@ -0,0 +1,20 @@
+import Queue from "bull";
+
+interface Payload {
+  orderId: string;
+}
+
+const expirationQueue = new Queue<Payload>("order:expiration", {
+  redis: {
+    host: process.env.REDIS_HOST,
+  },
+});
+
+expirationQueue.process(async (job) => {
+  console.log(
+    "Publish expiration:complete event for orderId:",
+    job.data.orderId
+  );
+});
+
+export { expirationQueue };
