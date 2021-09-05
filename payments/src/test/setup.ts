f84c0095e811e2ddec2648e9d9e0697b37c28fd5
Hung Nguyen Ba <nbhung189@gmail.com>
2021-09-05 18:57:43 +0700

    setup and test create charge route in payments service

--- payments/src/test/setup.ts
+++ payments/src/test/setup.ts
@@ -7,7 +7,7 @@ import { app } from "../app";
 declare global {
   namespace NodeJS {
     interface Global {
-      signup: () => string[];
+      signup: (id?: string) => string[];
     }
   }
 }
@@ -45,9 +45,9 @@ afterAll(async () => {
   await mongoose.connection.close();
 });
 
-global.signup = () => {
+global.signup = (id?: string) => {
   const payload = {
-    id: mongoose.Types.ObjectId().toHexString(),
+    id: id || mongoose.Types.ObjectId().toHexString(),
     email: "test@test.com",
   };
 
