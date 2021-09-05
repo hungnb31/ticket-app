3868d9ba189a6b6884e586979fd9cc2e1ca1012e
Hung Nguyen Ba <nbhung189@gmail.com>
2021-09-05 16:27:58 +0700

    setup order service

---
+++ payments/src/app.ts
@@ -0,0 +1,25 @@
+import express from "express";
+import "express-async-errors";
+import { json } from "body-parser";
+import cookieSession from "cookie-session";
+import { errorHandler, NotFoundError, currentUser } from "@ticketappdev/common";
+
+const app = express();
+
+app.set("trust proxy", 1);
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== "test",
+  })
+);
+app.use(json());
+app.use(currentUser);
+
+app.all("*", async () => {
+  throw new NotFoundError();
+});
+
+app.use(errorHandler);
+
+export { app };
---
+++ payments/src/test/setup.ts
@@ -0,0 +1,63 @@
+import { MongoMemoryServer } from "mongodb-memory-server";
+import mongoose from "mongoose";
+import jwt from "jsonwebtoken";
+
+import { app } from "../app";
+
+declare global {
+  namespace NodeJS {
+    interface Global {
+      signup: () => string[];
+    }
+  }
+}
+
+let mongo;
+
+jest.mock("../nats-wrapper");
+// init connection before test
+beforeAll(async () => {
+  // setup env
+  process.env.JWT_KEY = "testkey";
+
+  mongo = await MongoMemoryServer.create();
+  const mongoUri = await mongo.getUri();
+
+  await mongoose.connect(mongoUri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+});
+
+// clean mongodb before each test
+beforeEach(async () => {
+  jest.clearAllMocks();
+  const collections = await mongoose.connection.db.collections();
+
+  for (let collection of collections) {
+    await collection.deleteMany({});
+  }
+});
+
+// close connection after test
+afterAll(async () => {
+  await mongo.stop();
+  await mongoose.connection.close();
+});
+
+global.signup = () => {
+  const payload = {
+    id: new mongoose.Types.ObjectId().toHexString(),
+    email: "test@test.com",
+  };
+
+  const token = jwt.sign(payload, process.env.JWT_KEY);
+
+  const session = { jwt: token };
+
+  const sessionJSON = JSON.stringify(session);
+
+  const base64 = Buffer.from(sessionJSON).toString("base64");
+
+  return [`express:sess=${base64}`];
+};
