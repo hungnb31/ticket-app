e57d7e6f57871e373b3eb395acea8d8dd6536cfd
Hung Nguyen Ba <nbhung189@gmail.com>
2021-09-07 23:33:06 +0700

    update payment created listener

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -5,6 +5,7 @@ import { TicketCreatedListener } from "./events/listeners/ticket-created-listene
 import { TicketUpdatedListener } from "./events/listeners/ticket-updated-listener";
 import { ExpirationCompleteListener } from "./events/listeners/expiration-complete-listener";
 import { natsWrapper } from "./nats-wrapper";
+import { PaymentCreatedListener } from "./events/listeners/payment-created-listener";
 
 const start = async () => {
   if (!process.env.JWT_KEY) {
@@ -40,6 +41,7 @@ const start = async () => {
     new TicketCreatedListener(natsWrapper.client).listen();
     new TicketUpdatedListener(natsWrapper.client).listen();
     new ExpirationCompleteListener(natsWrapper.client).listen();
+    new PaymentCreatedListener(natsWrapper.client).listen();
     // -----------------
 
     await mongoose.connect(process.env.MONGO_URI, {
