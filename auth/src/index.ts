950417ccc5933cccaa96a3b5a996815f85e2ac5e
Hung Nguyen Ba <hungnb31@gmail.com>
2021-09-11 22:41:16 +0700

    update some changes

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,7 +3,7 @@ import { app } from "./app";
 import mongoose from "mongoose";
 
 const start = async () => {
-  console.log("Auth service starting up......");
+  console.log("Auth service is starting up...");
   if (!process.env.JWT_KEY) {
     throw new Error("JWT_KEY must be defined");
   }
