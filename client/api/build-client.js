f0f8b63abf68017bf28e0873425cff57e86247b5
Hung Nguyen Ba <hungnb31@gmail.com>
2021-09-12 11:56:01 +0700

    seperate dev and prod build and domain

--- client/api/build-client.js
+++ client/api/build-client.js
@@ -1,10 +1,15 @@
 import axios from "axios";
 
+const domain =
+  process.env.NODE_ENV === "production"
+    ? "http://bluutech.co"
+    : "http://ticketapp.dev";
+
 const buildClient = ({ req }) => {
   if (typeof window === "undefined") {
     // on the server
     return axios.create({
-      baseURL: "http://bluutech.co",
+      baseURL: domain,
       headers: req.headers,
     });
   } else {
